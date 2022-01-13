5c652b2d75cc6484f38b5780a7a695a45774876a
fraserc6 <fraserc6@gmail.com>
2022-01-13 15:48:35 +0000

    Deploying to gh-pages from @ nhsx/open-source-imaging-data-sets@52923c2cc3dcd9ff32d8a7ab171110a010feb0f0 🚀

--- _next/static/uTFFPATCor4JrQD_ocz5t/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/9-b3134a2b1154382b.js","static/chunks/pages/index-8c4bc1299e281cf0.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
