9dc97237603920531300109ea9953e16be767367
Fraser Cook <fraserc6@gmail.com>
2022-03-22 14:56:08 +0000

    Expect new data structure
    - Dynamically create array (opposed to setting each index)
    - Include data notes in table
    - Remove view button (the row should link itself)

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import Button from "components/Button"
 import Select from "components/Select"
 
 const Introduction = ({ active }) => (
-   <section className={`flex duration-500 ease-in-out ${active ? 'h-0' : 'h-[200px] sm:h-[250px] md:h-[300px] xl:h-[500px]'}`}>
+   <section className={`flex duration-500 ease-in-out ${active ? 'h-0 opacity-0' : 'h-[200px] sm:h-[250px] md:h-[300px] xl:h-[500px]'}`}>
       <div className="flex-1 flex flex-col justify-center py-16 sm:py-24 lg:py-32">
          <Container>
             <h1 className="text-3xl sm:text-5xl lg:text-6xl font-bold text-center tracking-tighter leading-tight text-gray-800">
@@ -33,24 +33,24 @@ const DataList = ({ search, setSearch, active, setActive }) => {
    const getData = async () => {
 
       // Parse file 
-      const data = await parseFile('/data/open-source-datasets-raw.csv')
+      const data = await parseFile('/data/snapshot-dataset.csv')
 
       // Remove headers
-      data.shift()
+      const headers = data.shift()
 
-      // Set data
-      setData(data.map(entry => ({
-         name: entry[0],
-         url: entry[1],
-         area: entry[2],
-         imageType: entry[3],
-         resourceType: entry[4],
-         focusType: entry[5],
-         openAccess: entry[6],
-         requestAccess: entry[7],
-         commercialAccess: entry[8],
-         dataType: entry[9],
-      })))
+      // Loop and create associative array 
+      let arr = []
+      for(let i = 0; i < data.length; i++) {
+         arr[i] = []
+         for(let j = 0; j < headers.length; j++) {
+            arr[i][headers[j].toLowerCase()] = data[i][j]
+         }
+      }
+
+      console.log(arr)
+
+      // Create array
+      setData(arr)
 
    }
 
@@ -62,7 +62,7 @@ const DataList = ({ search, setSearch, active, setActive }) => {
    return (
       <section className={`pt-2 pb-6 duration-500 ease-in-out`}>
          <Container>
-            <div className={`relative bg-white shadow-xl rounded-xl`}>
+            <div className={`relative bg-white border border-gray-100 shadow-xl rounded-xl`}>
                <div className="py-6 bg-white rounded-t-xl sticky top-0 px-6">
                   <div className="flex flex-col space-y-2 sm:space-y-0 sm:flex-row sm:space-x-4 sm:items-center">
                      <div className="relative flex-1">
@@ -168,7 +168,7 @@ const DataList = ({ search, setSearch, active, setActive }) => {
                                                 scope="col"
                                                 className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap"
                                              >
-                                                Image Types
+                                                Type
                                              </th>
                                              <th
                                                 scope="col"
@@ -182,47 +182,38 @@ const DataList = ({ search, setSearch, active, setActive }) => {
                                              >
                                                 Permissions
                                              </th>
-                                             <th scope="col" className="relative px-6 py-3">
-                                                <span className="sr-only">Edit</span>
-                                             </th>
                                           </tr>
                                        </thead>
                                        <tbody className="divide-y divide-gray-200">
-                                          {filteredData.map((entry) => (
-                                             <tr key={entry.url}>
+                                          {filteredData.map((entry, index) => (
+                                             <tr key={index}>
                                                 <td className="px-6 py-4">
-                                                   <Link href={entry.url}>
+                                                   <Link href={entry['url'] ?? '#'}>
                                                       <a target="_blank">
-                                                         <span className="block text-sm font-medium text-gray-900">{entry.name || '-'}</span>
-                                                         <span className="block text-sm text-gray-500">{entry.url}</span>
+                                                         <span className="block text-sm font-medium text-gray-900">{entry['name'] || '-'}</span>
+                                                         <span className="block text-sm text-blue-500">{entry['url']}</span>
+                                                         <span className="block text-sm text-gray-500 mt-2">{entry['data notes']}</span>
                                                       </a>
                                                    </Link>
                                                 </td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{entry.imageType}</td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{entry.focusType}</td>
+                                                <td className="px-6 py-4 text-sm text-gray-500">{entry['type of resource']}</td>
+                                                <td className="px-6 py-4 text-sm text-gray-500">{entry['area of focus']}</td>
                                                 <td className="px-6 py-4 flex flex-col space-y-2 items-start whitespace-nowrap">
-                                                   {entry.openAccess && (
+                                                   {entry['open access'] === "TRUE" && (
                                                       <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
                                                          Open Access
                                                       </span>
                                                    )}
-                                                   {entry.requestAccess && (
+                                                   {entry['access on application'] === "TRUE" && (
                                                       <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-100 text-blue-800">
-                                                         Access on Request
+                                                         Access on Application
                                                       </span>
                                                    )}
-                                                   {entry.openAccess && (
+                                                   {entry['commercial access'] === "TRUE" && (
                                                       <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
                                                          Commercial Access
                                                       </span>
                                                    )}
-                                                </td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                   <Link href={entry.url}>
-                                                      <a target="_blank" className="text-blue-600 hover:text-blue-900">
-                                                         View
-                                                      </a>
-                                                   </Link>
                                                 </td>
                                              </tr>
                                           ))}
@@ -253,29 +244,21 @@ export default function Home() {
    const [active, setActive] = useState(false)
 
    return (
-      <div className="relative flex flex-col">
+      <div className="relative flex flex-col min-h-screen bg-gray-50">
 
          {/* Pattern bg */}
-         <div className="absolute inset-0 z-[1] pointer-events-none bg-repeat bg-[length:50px_50px] opacity-5" style={{ backgroundImage: `url(${prefix}/hideout.svg)` }}></div>
+         <div className="absolute inset-0 z-[1] pointer-events-none bg-repeat bg-[length:50px_50px] opacity-[3%]" style={{ backgroundImage: `url(${prefix}/hideout.svg)` }}></div>
 
          {/* Above the fold */}
-         <div className="relative">
-            <div className="absolute inset-0 z-[0] pointer-events-none bg-gradient-to-b from-gray-50 to-gray-100"></div>
-            <div className="absolute inset-0 z-[2] pointer-events-none bg-gradient-to-b from-gray-50"></div>
-            <div className="relative z-[3] flex flex-col">
-               <Header />
-               <Introduction active={active} setActive={setActive} />
-            </div>
+         <div className="relative z-[3] flex flex-col">
+            <Header />
+            <Introduction active={active} setActive={setActive} />
          </div>
 
          {/* Data list */}
-         <div className="relative">
-            <div className="absolute inset-0 z-[0] pointer-events-none bg-gradient-to-b from-gray-100 to-gray-50"></div>
-            <div className="absolute inset-0 z-[2] pointer-events-none bg-gradient-to-b to-gray-50"></div>
-            <div className="relative z-[3]">
-               <DataList search={search} setSearch={setSearch} active={active} setActive={setActive} />
-               <Footer />
-            </div>
+         <div className="relative z-[3]">
+            <DataList search={search} setSearch={setSearch} active={active} setActive={setActive} />
+            <Footer />
          </div>
       </div>
    )
