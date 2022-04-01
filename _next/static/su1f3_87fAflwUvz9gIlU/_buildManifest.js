a7f4c1ccb5c3e416cd50da37660dc83916e011f0
amadeuspzs <amadeus.stevenson@gmail.com>
2022-04-01 10:16:22 +0000

    Deploying to gh-pages from @ nhsx/open-source-imaging-data-sets@37c73022c70f7968531700e5a59f8f7d347dc95b 🚀

---
+++ _next/static/su1f3_87fAflwUvz9gIlU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/842-9c1f4c240c3ccc76.js","static/chunks/pages/index-32ee09eb1387cada.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
